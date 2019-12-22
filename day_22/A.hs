a9f2fd9ac0a8ea60f3b53832f82939652771058e
Stephen Swatman <stephenswat@gmail.com>
2019-12-22 22:30:12 +0100

    Fix 22A

--- day_22/A.hs
+++ day_22/A.hs
@@ -3,15 +3,15 @@ import Data.Map ((!), fromList)
 import Data.List
 import Common
 
-applyShuffle :: [Int] -> Shuffle -> [Int]
+applyShuffle :: [Integer] -> Shuffle -> [Integer]
 applyShuffle d (Reverse) = reverse d
 applyShuffle d (Cut c)
-    | c < 0 = applyShuffle d (Cut (length d + c))
-    | otherwise = (drop c d) ++ (take c d)
+    | c < 0 = applyShuffle d (Cut (fromIntegral (length d) + c))
+    | otherwise = (drop (fromIntegral c) d) ++ (take (fromIntegral c) d)
 applyShuffle d (Deal c) = map (m !) [0..l - 1]
     where
         l = length d
-        m = fromList [((i * c) `mod` l, d !! i) | i <- [0..l - 1]]
+        m = fromList [((i * (fromInteger c)) `mod` l, d !! i) | i <- [0..l - 1]]
 
 main = do {
     [f] <- getArgs;
@@ -19,10 +19,8 @@ main = do {
     let 
         i = parseShuffles s;
         d = [0..10006]
-        -- r = foldl applyShuffle d i
-        r = foldl applyShuffle [0..9] [Cut (-2)]
+        r = foldl applyShuffle d i
     in do {
-        print r;
-        -- print (findIndex (== 2019) r);
+        print (findIndex (== 2019) r);
     }
 }
